34dcc86f1ea1b23dee8617ff8157eadaf48a165b
Atul Bhosale <atul1bhosale@gmail.com>
2015-10-18 07:39:08 +0530

    Added Paperclip image validator

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,6 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   belongs_to :album
 
-  has_attached_file :image, default_url: "/images/medium/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>"}, default_url: "/images/medium/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
